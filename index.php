3dc85564a4b8fe29df290c864d4e89c119d901e3
Huskarl10 <martinmal@uia.no>
2024-10-15 15:10:06 +0200

    La inn form

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
   <label for="tabone">Registrering</label>
   <div class="tab">
     <h2>Registrering</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <form action="/registrering_page.php" target="_blank">
+      <p><input class="user_name" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="user_email" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="user_message" type="text" placeholder="Message" required name="Message"></p>
+      <p><button class="user_submit" type="submit">SEND MELDING</button></p>
+    </form>
   </div>
   
   <input type="radio" name="tabs" id="tabtwo">
